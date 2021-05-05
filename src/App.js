d7859a4f5c73ece19f7c5b12d96b53ca9b43edee
Winston Chen <winstonchen.wm@gmail.com>
2021-05-05 00:46:21 -0400

    Search

--- src/App.js
+++ src/App.js
@@ -10,23 +10,26 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchText, setSearchText] = useState('')
 
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=d1684ae3"
+  const getMovieRequest = async (searchText) => {
+    const url = `http://www.omdbapi.com/?s=${searchText}&apikey=d1684ae3`
 
     const response = await fetch(url);
     const responseJSON = await response.json();
-    setMovies(responseJSON.Search)
+
+    if (responseJSON.Search) {
+      setMovies(responseJSON.Search)
+    }
   }
 
   useEffect(()=>{
-    getMovieRequest();
-  }, [])
+    getMovieRequest(searchText);
+  }, [searchText])
 
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
         <Heading heading="The Shoppies"/>
-        <Search />
+        <Search searchText={searchText} setSearchText={setSearchText}/>
       </div>
       <div className="row">
         <Movies movies={movies}/>
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Search(props) {
     return (
         <div className="col col-sm-4">
-            <input className="form-control" placeholder="Search"></input>
+            <input className="form-control" value={props.value} onChange={(event)=> props.setSearchText(event.target.value)} placeholder="Search"></input>
         </div>
     )
 }
