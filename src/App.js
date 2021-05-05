df48abe98f5f13f57adbe4d15c378cba5b74584f
Winston Chen <winstonchen.wm@gmail.com>
2021-05-04 23:58:07 -0400

    Heading

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Movies from './Components/Movies';
+import Heading from './Components/Heading';
+import Search from './Components/Search';
 
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchText, setSearchText] = useState('')
 
   const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=nba&apikey=d1684ae3"
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=d1684ae3"
 
     const response = await fetch(url);
     const responseJSON = await response.json();
@@ -21,6 +24,10 @@ function App() {
 
   return (
     <div className='container-fluid movie-app'>
+      <div className='row'>
+        <Heading heading="The Shoppies"/>
+        <Search />
+      </div>
       <div className="row">
         <Movies movies={movies}/>
       </div>
---
+++ src/Components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search(props) {
+    return (
+        <div className="col col-sm-4">
+            <input className="form-control" placeholder="Search"></input>
+        </div>
+    )
+}
+
+export default Search
